441756978e325db917a9809acd17b786854a044f
Ryan Kim <skim19@stuy.edu>
2017-11-13 15:56:56 -0500

    Added Default Stats for Ogre_Priest Class

--- v3/Ogre_Priest.java
+++ v3/Ogre_Priest.java
@@ -1,7 +1,20 @@
 public class Ogre_Priest extends Monster{
-    
+
+   public Ogre_Priest(){
+	name = "Night Knight";
+	health = 150;
+	attack = 1;
+	defense = 60;
+	strength = (int) ( (Math.random() * 20) + 45 );
+    }
+
+    public Ogre_Priest( String entName ){
+	this();
+	name = entName;
+    }    
+
     public static void about(){
-	System.out.println("This monster has low attack and high defense. It also has moderate HP. It can heal itself every turn.");
+	System.out.println("This monster has low health and low attack but high defense. It also has moderate HP. It can heal itself every turn.");
     }
 
     public static void main(String[] args){
