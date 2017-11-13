3fd5537acccd096394d29bb0757e4be481f89eda
Ryan Kim <skim19@stuy.edu>
2017-11-13 15:51:20 -0500

    Added Default Stats for Knight Class

--- v3/Knight.java
+++ v3/Knight.java
@@ -1,7 +1,20 @@
 public class Knight extends Protagonist{
     
+    public Knight(){
+	name = "Night Knight";
+	health = 350;
+	attack = 1;
+	defense = 40;
+	strength = (int) ( (Math.random() * 20) + 45 );
+    }
+
+    public Knight( String entName ){
+	this();
+	name = entName;
+    }
+    
     public static void about(){
-	System.out.println("This tank class has high defense and low attack power. He has no regeneration.");
+	System.out.println("This tank class has high defense and high health but low attack power. He has no regeneration.");
     }
 
     public static void main (String[] args){
