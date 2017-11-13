6a41eb7be60fb5febf3e865a9afe07042b2192a2
Ryan Kim <skim19@stuy.edu>
2017-11-13 15:46:22 -0500

    Changed file name, set Default Stats for Golem subclass

--- v3/Berserker.java
+++ v3/Berserker.java
@@ -1,7 +1,20 @@
-public class Berserker extends Monster{
+public class Golem extends Monster{
+ 
+    public Golem(){
+	name = "GoGoGolem";
+	health = 350;
+	attack = 1;
+	defense = 40;
+	strength = (int) ( (Math.random() * 20) + 45 );
+    }
+
+    public Monster( String entName ){
+	this();
+	name = entName;
+    }
     
     public static void about(){
-	System.out.println("This monster has high attack, but low defenses. It also has low HP.");
+	System.out.println("This monster has low attack, but high defenses. It also has high HP.");
     }
 
     public static void main(String[] args){
